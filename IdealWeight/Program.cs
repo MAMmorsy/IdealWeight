060db19519b9b1ddc536ea43530e121b2405198e
MAMmorsy <mohamed.morsy@egyptlinx.com>
2021-09-20 13:58:29 +0200

    Version 2

--- IdealWeight/Program.cs
+++ IdealWeight/Program.cs
@@ -9,9 +9,32 @@ namespace IdealWeight
             //Console.WriteLine("Hello World!");
             WeightCalculator wegihtcalc = new WeightCalculator
             { Height=180,
-            Gender='f'};
+            Gender='m'};
             double weight = wegihtcalc.GetIdealBodyWeight();
             Console.WriteLine($"The ideal weight is {weight}");
+            if(weight==72.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error!");
+            }
+            wegihtcalc.Gender = 'f';
+            weight = wegihtcalc.GetIdealBodyWeight();
+            Console.WriteLine($"The ideal weight is {weight}");
+            if (weight == 72.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error!");
+            }
             Console.ReadKey();
         }
     }
