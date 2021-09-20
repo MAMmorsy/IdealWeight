48ceb01f69f8ec615e955e970348b53768f7558c
MAMmorsy <mohamed.morsy@egyptlinx.com>
2021-09-20 13:37:40 +0200

    Version 1

--- IdealWeight/Program.cs
+++ IdealWeight/Program.cs
@@ -6,7 +6,13 @@ namespace IdealWeight
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            WeightCalculator wegihtcalc = new WeightCalculator
+            { Height=180,
+            Gender='f'};
+            double weight = wegihtcalc.GetIdealBodyWeight();
+            Console.WriteLine($"The ideal weight is {weight}");
+            Console.ReadKey();
         }
     }
 }
---
+++ IdealWeight/WeightCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdealWeight
+{
+    public class WeightCalculator
+    {
+        public double Height { get; set; }
+        public char Gender { get; set; }
+
+        public double GetIdealBodyWeight()
+        {
+            switch (Gender)
+            {
+                case 'm':
+                    return (Height - 100) - ((Height - 150) / 4);
+                case 'f':
+                    return (Height - 100) - ((Height - 150) / 2);
+
+                default:
+                   return 0;
+            }
+        }
+    }
+}
